62a8545e1a95fc5cfde04a2e99229e8c0b0d51a6
QCare Dev <qcaredev@gmail.com>
2025-07-08 18:28:21 -0500

    * Updated for deployment fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,9 +2,9 @@
 Contains the Stripe configuration for the app.
 */
 
-import Stripe from "stripe"
+import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2025-02-24.acacia",
   appInfo: { name: "Receipt AI", version: "0.1.0" }
 })
